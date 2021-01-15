039abc452399e8b47ccbfde40b295640af1878b7
Konstantin Brovtsev <brovan@yandex.ru>
2021-01-15 16:29:23 +0300

    fix: max-width in profile

--- script/edit-profile.js
+++ script/edit-profile.js
@@ -24,12 +24,6 @@ function formSubmit(evt){
   profileJob.textContent=formJob.value;
 }
 
-//let profileName = document.querySelector('.profile__name');
-//let profileJob = document.querySelector('.profile__job');
-
-//let formName = document.querySelector('.popup__form-name');
-//let formJob = document.querySelector('.popup__job-name');
-
 let formElement = document.querySelector('.popup__container');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = formElement.querySelector('.popup__close-button');
